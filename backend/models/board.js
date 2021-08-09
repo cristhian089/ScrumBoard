ee8a93e7a56a1a85d26341c403d8dd68fcc1f96f
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-09 14:44:25 -0500

    adding model,routes and controladores

---
+++ backend/models/board.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const boardSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.ObjectId, ref:"user"},
+    name: String,
+    description: String,
+    taskStatus: String,
+    imageURL: String,
+    date: {type: Date, default: Date.now}, 
+});
+
+const board = mongoose.model("board",boardSchema);
+module.exports = board;
